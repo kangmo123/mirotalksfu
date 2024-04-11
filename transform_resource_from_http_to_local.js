ac89d9f1efb2d5926233ed5e416b671046d2fa06
kangmo123 <kangmo123@126.com>
2024-04-11 22:29:15 +0800

    feat: test

--- transform_resource_from_http_to_local.js
+++ transform_resource_from_http_to_local.js
@@ -19,28 +19,29 @@ files.forEach(file => {
   const htmlContent = fs.readFileSync(path.join(sourcePath, file), 'utf-8');
   const $ = cheerio.load(htmlContent);
 
-  function replaceHttp(tagName, attributeName, folder) {
+  function replaceHttp(tagName, attributeName, folder, public = undefined) {
     const scriptTags = $(`${tagName}[${attributeName}]`);
     scriptTags.each((index, element) => {
       const attributeValue = $(element).attr(attributeName);
       if (attributeValue.startsWith('https://')) {
         const localPath = path.join(__dirname, `${resultPath}/${folder}`, attributeValue.substring(8));
+        const publicPath = path.join(__dirname, `${resultPath}`, `${public}`, `${folder}`, attributeValue.substring(8));
         const localDir = path.dirname(localPath);
     
         if (!fs.existsSync(localDir)) {
           fs.mkdirSync(localDir, { recursive: true });
         }
     
-        $(element).attr(attributeName, '.' + path.relative('.', localPath).substring(resultPath.length));
+        $(element).attr(attributeName, '.' + path.relative('.', publicPath).substring(resultPath.length));
         request(attributeValue).pipe(fs.createWriteStream(localPath))
       }
     });
   }
 
-  replaceHttp('script', 'src', 'views/js')
-  replaceHttp('link', 'href', 'views/css')
-  replaceHttp('meta', 'content', 'views/meta')
-  replaceHttp('img', 'src', 'views/img')
+  replaceHttp('script', 'src', 'js', 'views')
+  replaceHttp('link', 'href', 'css', 'views')
+  replaceHttp('meta', 'content', 'meta', 'views')
+  replaceHttp('img', 'src', 'img', 'views')
 
   fs.writeFileSync(path.join(resultPath, file), $.html());
 })
